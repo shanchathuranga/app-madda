7ce657ec1b9932127442d53385ab64043a1dbd2f
shanchathuranga <shanchathuranga@9b92a12a-af4e-d7b3-4346-7b88fbde7260>
2011-08-21 04:59:11 +0000

    Test files committed

---
+++ servicestation/src/com/flash/system/ui/MainWindow.java
@@ -0,0 +1,19 @@
+package com.flash.system.ui;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author shan
+ */
+public class MainWindow extends JFrame{
+
+    public MainWindow() {
+        super();
+        setSize(500, 500);
+        setTitle("Flash Service Station");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+}
