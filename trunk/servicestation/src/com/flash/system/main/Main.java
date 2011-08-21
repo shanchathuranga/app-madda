348a620f60c0fa5a96c07a8bc1bdfdbf578a37b9
shanchathuranga <shanchathuranga@9b92a12a-af4e-d7b3-4346-7b88fbde7260>
2011-08-21 04:59:11 +0000

    Test files committed

---
+++ trunk/servicestation/src/com/flash/system/main/Main.java
@@ -0,0 +1,17 @@
+package com.flash.system.main;
+
+import com.flash.system.ui.MainWindow;
+
+/**
+ *
+ * @author shan
+ */
+public class Main {
+
+    public static void main(String args[]) {
+
+        new MainWindow();
+
+    }
+
+}
