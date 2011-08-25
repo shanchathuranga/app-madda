274eceaa735676ab39141b467a636a92f753e6e9
shanchathuranga <shanchathuranga@9b92a12a-af4e-d7b3-4346-7b88fbde7260>
2011-08-25 15:26:36 +0000

    Adding methods to CustomerDAO interface

--- servicestation/src/com/flash/system/core/service/CustomerDAOImpl.java
+++ servicestation/src/com/flash/system/core/service/CustomerDAOImpl.java
@@ -1,11 +1,32 @@
 package com.flash.system.core.service;
 
 import com.flash.system.core.dao.CustomerDAO;
+import java.util.Collection;
 
 /**
  *
  * @author shan
  */
 public class CustomerDAOImpl implements CustomerDAO {
+
+    public void add(Customer customer) {
+
+    }
+
+    public void update(Customer customer) {
+
+    }
+
+    public void remove(Customer customer) {
+
+    }
+
+    public Customer findByPrimaryKey(Long customerId) {
+
+    }
+
+    public Collection findAll() {
+        
+    }
 
 }
