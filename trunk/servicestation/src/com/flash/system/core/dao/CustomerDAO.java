549684241bb2fc70df3ae74f719f5c4c722bed51
shanchathuranga <shanchathuranga@9b92a12a-af4e-d7b3-4346-7b88fbde7260>
2011-08-25 15:23:11 +0000

    Adding methods to CustomerDAO interface

--- trunk/servicestation/src/com/flash/system/core/dao/CustomerDAO.java
+++ trunk/servicestation/src/com/flash/system/core/dao/CustomerDAO.java
@@ -1,9 +1,17 @@
 package com.flash.system.core.dao;
 
+import java.util.Collection;
+
 /**
  *
  * @author shan
  */
 public interface CustomerDAO {
+
+    public void add(Customer customer);
+    public void update(Customer customer);
+    public void remove(Customer customer);
+    public Customer findByPrimaryKey(Long id);
+    public Collection findAll();
 
 }
