4ae8d8abc7dbab486beca9c3fba0916b72c20732
shanchathuranga <shanchathuranga@9b92a12a-af4e-d7b3-4346-7b88fbde7260>
2011-08-26 06:23:38 +0000

    update mainwindow.java

--- trunk/servicestation/src/com/flash/system/view/MainWindow.java
+++ trunk/servicestation/src/com/flash/system/view/MainWindow.java
@@ -1,10 +1,16 @@
 package com.flash.system.view;
 
+import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 
@@ -14,6 +20,18 @@ import javax.swing.UIManager;
  */
 public class MainWindow extends JFrame{
 
+    private JMenuBar menubar;
+
+    private JMenu menuApp;
+    private JMenu menuCustomer;
+    private JMenu menuVehicle;
+
+    private JMenuItem menuItemExit;
+    private JMenuItem menuItemAddCustomer;
+    private JMenuItem menuItemUpdateCustomer;
+    private JMenuItem menuItemRegisterVehicle;
+    private JMenuItem menuItemUpdateVehicle;
+
     public MainWindow() {
         super();
         try {
@@ -21,8 +39,9 @@ public class MainWindow extends JFrame{
         } catch (Exception ex) {
             System.out.println(ex);
         }
+        setDefaultLookAndFeelDecorated(true);
 
-        JPanel panel = new JPanel();
+        /*JPanel panel = new JPanel();
         getContentPane().add(panel);
 
         panel.setLayout(null);
@@ -34,14 +53,89 @@ public class MainWindow extends JFrame{
             }
         });
         bt.setToolTipText("Quit");
-        panel.add(bt);
+        panel.add(bt);*/
+
+        initMenuBar();
+        initMenuActions();
 
         setSize(Toolkit.getDefaultToolkit().getScreenSize().width,
                 Toolkit.getDefaultToolkit().getScreenSize().height
                 );
-        setLocationRelativeTo(null);
+        setIconImage(getFDImage());
+        //setLocationRelativeTo(null);
         setTitle("Flash Service Station");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    protected static Image getFDImage() {
+        java.net.URL imgURL = MainWindow.class.getResource("/com/flash/system/resource/car_icon.png");
+        if (imgURL != null) {
+            return new ImageIcon(imgURL).getImage();
+        } else {
+            return null;
+        }
+    }
+
+    private void initMenuBar() {
+        menubar = new JMenuBar();
+
+        /* Creating Menus */
+        menuApp = new JMenu("Application");
+        menuCustomer = new JMenu("Customer Mgt");
+        menuVehicle = new JMenu("Vehicle Mgt");
+
+        /* Creating Menu Items */
+        menuItemExit = new JMenuItem("Exit");
+        menuItemAddCustomer = new JMenuItem("Add Customer");
+        menuItemUpdateCustomer = new JMenuItem("Update Customer");
+        menuItemRegisterVehicle = new JMenuItem("Register Vehicle");
+        menuItemUpdateVehicle = new JMenuItem("Update Vehicle");
+
+        /* Adding Menu Items to Menus */
+        menuApp.add(menuItemExit);
+        menuCustomer.add(menuItemAddCustomer);
+        menuCustomer.add(menuItemUpdateCustomer);
+        menuVehicle.add(menuItemRegisterVehicle);
+        menuVehicle.add(menuItemUpdateVehicle);
+
+        /* Adding Menus to the Menubar */
+        menubar.add(menuApp);
+        menubar.add(menuCustomer);
+        menubar.add(menuVehicle);
+
+        setJMenuBar(menubar);
+    }
+
+    private void initMenuActions() {
+        menuItemExit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                System.exit(0);
+            }
+        });
+
+        menuItemAddCustomer.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                JOptionPane.showMessageDialog(null, "Not supported yet.");
+            }
+        });
+
+        menuItemUpdateCustomer.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                JOptionPane.showMessageDialog(null, "Not supported yet.");
+            }
+        });
+
+        menuItemRegisterVehicle.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                JOptionPane.showMessageDialog(null, "Not supported yet.");
+            }
+        });
+
+        menuItemUpdateVehicle.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                JOptionPane.showMessageDialog(null, "Not supported yet.");
+            }
+        });
     }
 
 }
