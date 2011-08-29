5b054f23f801cb9a2718626e8a390fc462253bae
sampathzone <sampathzone@9b92a12a-af4e-d7b3-4346-7b88fbde7260>
2011-08-29 15:17:28 +0000

    Adding Hibernate class body

--- servicestation/src/com/flash/system/util/HibernateUtil.java
+++ servicestation/src/com/flash/system/util/HibernateUtil.java
@@ -1,9 +1,42 @@
 package com.flash.system.util;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.classic.Session;
+
 /**
  *
  * @author shan
  */
 public class HibernateUtil {
 
+    public static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            Configuration config = new Configuration().configure();
+            sessionFactory = config.buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    public static final ThreadLocal session = new ThreadLocal();
+
+    public static Session currentSession() throws Exception {
+        Session s = (Session) session.get();
+        if (s == null) {
+            s = sessionFactory.openSession();
+            session.set(s);
+        }
+        return s;
+    }
+
+    public static void closeSession() throws Exception {
+        Session s = (Session) session.get();
+        if (s != null) {
+            s.close();
+        }
+        session.set(null);
+    }
 }
