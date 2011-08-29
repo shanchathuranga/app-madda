35704345e9e231aca905970b18ea85b88f81e365
shanchathuranga <shanchathuranga@9b92a12a-af4e-d7b3-4346-7b88fbde7260>
2011-08-29 17:17:17 +0000

    Testing hibernate, Creating Customer table, Creating DB

--- trunk/servicestation/src/com/flash/system/main/Main.java
+++ trunk/servicestation/src/com/flash/system/main/Main.java
@@ -1,7 +1,12 @@
 package com.flash.system.main;
 
+import com.flash.system.core.entity.Customer;
 import com.flash.system.view.MainWindow;
 import javax.swing.SwingUtilities;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.Session;
+import org.hibernate.tool.hbm2ddl.SchemaExport;
 
 /**
  *
@@ -10,6 +15,14 @@ import javax.swing.SwingUtilities;
 public class Main {
 
     public static void main(String args[]) {
+
+        System.out.println("Running DB queries...");
+
+        AnnotationConfiguration config = new AnnotationConfiguration();
+        config.addAnnotatedClass(Customer.class);
+        config.configure();
+
+        new SchemaExport(config).create(true, true);
 
         System.out.println("Starting...");
 
