26f0cd8e541e82d91743c783b5bf900f86c25e79
shanchathuranga <shanchathuranga@9b92a12a-af4e-d7b3-4346-7b88fbde7260>
2011-08-31 18:45:38 +0000

    VehicleCategory, VehicleModel, VehicleType entities added

---
+++ servicestation/src/com/flash/system/core/entity/VehicleCategory.java
@@ -0,0 +1,52 @@
+package com.flash.system.core.entity;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author shan
+ */
+@Entity
+public class VehicleCategory implements Serializable {
+
+    private int categoryCode;
+    private String categoryName;
+    private List<Vehicle> vehicles;
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    public int getCategoryCode() {
+        return categoryCode;
+    }
+
+    public void setCategoryCode(int categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    @OneToMany(targetEntity=Vehicle.class, mappedBy="vehicleModel",
+        cascade=CascadeType.ALL, fetch=FetchType.LAZY)
+    public List<Vehicle> getVehicles() {
+        return vehicles;
+    }
+
+    public void setVehicles(List<Vehicle> vehicles) {
+        this.vehicles = vehicles;
+    }
+
+}
---
+++ servicestation/src/com/flash/system/core/entity/VehicleModel.java
@@ -0,0 +1,52 @@
+package com.flash.system.core.entity;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author shan
+ */
+@Entity
+public class VehicleModel implements Serializable {
+
+    private int modelCode;
+    private String modelName;
+    private List<Vehicle> vehicles;
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    public int getModelCode() {
+        return modelCode;
+    }
+
+    public void setModelCode(int modelCode) {
+        this.modelCode = modelCode;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    @OneToMany(targetEntity=Vehicle.class, mappedBy="vehicleModel",
+        cascade=CascadeType.ALL, fetch=FetchType.LAZY)
+    public List<Vehicle> getVehicles() {
+        return vehicles;
+    }
+
+    public void setVehicles(List<Vehicle> vehicles) {
+        this.vehicles = vehicles;
+    }
+    
+}
