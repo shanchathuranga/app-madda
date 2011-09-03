0c3e2f87325f8650a266379fe6bb8326a303cff3
shanchathuranga <shanchathuranga@9b92a12a-af4e-d7b3-4346-7b88fbde7260>
2011-09-03 17:04:13 +0000

    Job / Repair Entity classes deleted

--- servicestation/src/com/flash/system/main/Main.java
+++ servicestation/src/com/flash/system/main/Main.java
@@ -6,11 +6,9 @@ import com.flash.system.core.entity.Customer;
 import com.flash.system.core.entity.Employee;
 import com.flash.system.core.entity.EmployeeType;
 import com.flash.system.core.entity.Item;
-import com.flash.system.core.entity.Job;
 import com.flash.system.core.entity.Order;
 import com.flash.system.core.entity.Permission;
 import com.flash.system.core.entity.PreOrder;
-import com.flash.system.core.entity.Repair;
 import com.flash.system.core.entity.Service;
 import com.flash.system.core.entity.Supplier;
 import com.flash.system.core.entity.Vehicle;
@@ -32,37 +30,35 @@ public class Main {
 
         System.out.println("Running DB queries...");
 
-//        AnnotationConfiguration config = new AnnotationConfiguration();
-//        config.addAnnotatedClass(Account.class);
-//        config.addAnnotatedClass(Bill.class);
-//        config.addAnnotatedClass(Customer.class);
-//        config.addAnnotatedClass(Employee.class);
-//        config.addAnnotatedClass(EmployeeType.class);
-//        config.addAnnotatedClass(Item.class);
-//        config.addAnnotatedClass(Job.class);
-//        config.addAnnotatedClass(Order.class);
-//        config.addAnnotatedClass(Permission.class);
-//        config.addAnnotatedClass(PreOrder.class);
-//        config.addAnnotatedClass(Repair.class);
-//        config.addAnnotatedClass(Service.class);
-//        config.addAnnotatedClass(Supplier.class);
-//        config.addAnnotatedClass(Vehicle.class);
-//        config.addAnnotatedClass(VehicleCategory.class);
-//        config.addAnnotatedClass(VehicleModel.class);
-//        config.addAnnotatedClass(VehicleType.class);
-//
-//        config.configure();
-//
-//        new SchemaExport(config).create(true, true);
+        AnnotationConfiguration config = new AnnotationConfiguration();
+        config.addAnnotatedClass(Account.class);
+        config.addAnnotatedClass(Bill.class);
+        config.addAnnotatedClass(Customer.class);
+        config.addAnnotatedClass(Employee.class);
+        config.addAnnotatedClass(EmployeeType.class);
+        config.addAnnotatedClass(Item.class);
+        config.addAnnotatedClass(Order.class);
+        config.addAnnotatedClass(Permission.class);
+        config.addAnnotatedClass(PreOrder.class);
+        config.addAnnotatedClass(Service.class);
+        config.addAnnotatedClass(Supplier.class);
+        config.addAnnotatedClass(Vehicle.class);
+        config.addAnnotatedClass(VehicleCategory.class);
+        config.addAnnotatedClass(VehicleModel.class);
+        config.addAnnotatedClass(VehicleType.class);
 
-        System.out.println("Application Starting...");
+        config.configure();
 
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                MainWindow mainWindow = new MainWindow();
-                mainWindow.setVisible(true);
-            }
-        });
+        new SchemaExport(config).create(true, true);
+
+//        System.out.println("Application Starting...");
+//
+//        SwingUtilities.invokeLater(new Runnable() {
+//            public void run() {
+//                MainWindow mainWindow = new MainWindow();
+//                mainWindow.setVisible(true);
+//            }
+//        });
 
     }
 
