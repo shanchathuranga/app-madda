8e8ee522721706bc0d0a85d93ce5156a6a554cae
sampathzone <sampathzone@9b92a12a-af4e-d7b3-4346-7b88fbde7260>
2011-09-08 14:18:34 +0000

    Adding permission util [ shan ]

---
+++ servicestation/src/com/flash/system/util/Permission.java
@@ -0,0 +1,18 @@
+package com.flash.system.util;
+
+/**
+ *
+ * @author sampath
+ */
+public class Permission {
+
+    public static enum PERM {
+        PERM_ANY,
+        PERM_MAN,
+        PERM_ITH,
+        PERM_STR,
+        PERM_ACC,
+        PERM_LOW
+    }
+
+}
